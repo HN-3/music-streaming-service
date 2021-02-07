fb3e44580ff87485b561ab4a8d7cefe452af593c
HYEEWON <hw0916@naver.com>
2021-02-07 22:23:00 +0900

    Update hestagram

---
+++ hestagram/hestagram/src/test/java/hestagram/hestagram/HestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package hestagram.hestagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HestagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
