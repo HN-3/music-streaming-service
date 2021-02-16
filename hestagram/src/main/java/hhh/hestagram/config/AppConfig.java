091a3d4f40efa7804ed00f3727794d5a33faf3b5
Haneul Bae <haneul@Haneului-MacBookPro.local>
2021-02-16 17:23:32 +0900

    add gitignore file

---
+++ hestagram/src/main/java/hhh/hestagram/config/AppConfig.java
@@ -0,0 +1,13 @@
+package hhh.hestagram.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:configure.properties")
+})
+public class AppConfig {
+}
