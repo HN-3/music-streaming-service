4b3aa098405e49ffb5872743639ba3191daa1d50
HYEEWON <hw0916@naver.com>
2021-02-26 14:19:32 +0900

    Update all

--- hestagram/src/main/java/hhh/hestagram/Content/ContentController.java
+++ hestagram/src/main/java/hhh/hestagram/Content/ContentController.java
@@ -1,9 +1,18 @@
 package hhh.hestagram.Content;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ContentController {
 
+    private final ContentService contentService;
+
+    @Autowired
+    public ContentController(ContentService contentService) {
+        this.contentService = contentService;
+    }
+
+    
 
 }
