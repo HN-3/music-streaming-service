e74c91fac5eaa26d6a6f007c6dfe044e048125ca
HYEEWON <hw0916@naver.com>
2021-02-26 20:10:49 +0900

    Update all

--- hestagram/src/main/java/hhh/hestagram/Content/ContentController.java
+++ hestagram/src/main/java/hhh/hestagram/Content/ContentController.java
@@ -1,7 +1,6 @@
 package hhh.hestagram.Content;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +19,7 @@ public class ContentController {
 
     @PostMapping("/contents/new") //{nickname}
     public Long createPost(@RequestBody ContentSaveRequestDto contentSaveRequestDto) {
+        System.out.println();
         return contentService.saveContent(contentSaveRequestDto);
     }
     
--- hestagram/src/main/java/hhh/hestagram/Content/ContentSaveRequestDto.java
+++ hestagram/src/main/java/hhh/hestagram/Content/ContentSaveRequestDto.java
@@ -9,16 +9,14 @@ import java.util.Date;
 @Getter
 @NoArgsConstructor
 public class ContentSaveRequestDto {
-    private Long userContentIndex;
     private String photo;
     private String contentText;
     private Long likeCount;
     private User user;
 
     @Builder
-    public ContentSaveRequestDto(Long userContentIndex, String photo,
-                                 String contentText, Long likeCount, User user) {
-        this.userContentIndex = userContentIndex;
+    public ContentSaveRequestDto(String photo, String contentText,
+                                 Long likeCount, User user) {
         this.photo = photo;
         this.contentText = contentText;
         this.likeCount = likeCount;
@@ -27,7 +25,6 @@ public class ContentSaveRequestDto {
 
     public Content toEntity() {
         return Content.builder()
-                .userContentIndex(userContentIndex)
                 .photo(photo)
                 .contentText(contentText)
                 .likeCount(likeCount)
